5a4020bbad55ef9712db72c7bec822b706571321
shouta0715 <Myheroes0715@gmail.com>
2023-11-26 19:36:55 +0900

    add wikimedia env

--- app/bootstrap/env.go
+++ app/bootstrap/env.go
@@ -5,11 +5,13 @@ import (
 )
 
 type Env struct {
-	ENVIRONMENT    string `mapstructure:"ENVIRONMENT"`
-	DBSource       string `mapstructure:"DB_SOURCE"`
-	MigrationURL   string `mapstructure:"MIGRATION_URL"`
-	ServerAddress  string `mapstructure:"SERVER_ADDRESS"`
-	ContextTimeout int    `mapstructure:"CONTEXT_TIMEOUT"`
+	ENVIRONMENT       string `mapstructure:"ENVIRONMENT"`
+	DBSource          string `mapstructure:"DB_SOURCE"`
+	MigrationURL      string `mapstructure:"MIGRATION_URL"`
+	ServerAddress     string `mapstructure:"SERVER_ADDRESS"`
+	ContextTimeout    int    `mapstructure:"CONTEXT_TIMEOUT"`
+	WikimediaUserName string `mapstructure:"WIKIMEDIA_USERNAME"`
+	WikimediaPassword string `mapstructure:"WIKIMEDIA_PASSWORD"`
 }
 
 func NewEnv(path string) (env Env, err error) {
